aea2b6cf0e66644d7ef6953d625f1ce911fd5d53
LucasRibeiroRJBR <lucasribeirorjbr@gmail.com>
2020-12-16 00:35:55 -0300

    Sons vogais

--- audio.js
+++ audio.js
@@ -1,1 +1,5 @@
-var a = new Audio(); a.src = "audio/a.mp3"; function play_a() {a.play();}
+var a = new Audio(); a.src = "audio/a.mp3"; function play_a() { a.play(); }
+var o = new Audio(); o.src = "audio/o.mp3"; function play_o() { o.play(); }
+var oh = new Audio(); oh.src = "audio/oh.mp3"; function play_oh() { oh.play(); }
+var u = new Audio(); u.src = "audio/u.mp3"; function play_u() { u.play(); }
+var uh = new Audio(); uh.src = "audio/uh.mp3"; function play_uh() { uh.play(); }
